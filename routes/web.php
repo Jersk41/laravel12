22ecffd5b88d1b95a1fb263cb14de34146d42ee5
Japar Sidik <japarssidik820@gmail.com>
2025-08-14 13:18:43 +0700

    View Sederhana dan Views Templating

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Models\Barang;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', ['name' => 'Jane Doe']);
+});
+
+Route::get('/barang', function () {
+    return view('data', [
+        'title' => 'Barang',
+        'items' => Barang::all(),
+    ]);
 });
